81817cd9e6b1803f360a58fcd92757ca1c03f6da
Santiago Vanegas <santiago.vanegas@ideaware.co>
2022-09-19 23:41:09 -0500

    stripe checkout session

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,28 @@
+class CheckoutController < ApplicationController
+    
+    def create
+        product = Product.find(params[:id])
+        @session = Stripe::Checkout::Session.create({
+            payment_method_types: ['card'],
+            line_items: [
+                price_data: {
+                    unit_amount: product.price,
+                    currency: "usd",
+                    product_data: {
+                        name: product.name,
+                        description: 'test description',
+                    }
+                },
+                quantity: 1
+            ],
+            mode: 'payment',
+            success_url: root_url,
+            cancel_url: root_url,
+        })
+
+        respond_to do |format|
+            format.js
+        end
+    end
+    
+end
