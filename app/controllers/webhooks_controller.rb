31a9371c43d71ff381dcb2ea36bf1805e6ae9d9c
Santiago Vanegas <santiago.vanegas@ideaware.co>
2022-09-21 21:50:59 -0500

    fix call credentials webhook

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -8,7 +8,7 @@ class WebhooksController < ApplicationController
 
        begin
         event = Stripe::Webhook.construct_event(
-            payload, sig_header, Rails.application.credentials[:Stripe][:Webhook]
+            payload, sig_header, Rails.application.credentials[:stripe][:webhook]
         )
        rescue JSON::ParserError => e
         status 400
