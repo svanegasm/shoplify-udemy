7e59c99ad85ba60a1ef02c53071fea5052e20159
Santiago Vanegas <santiago.vanegas@ideaware.co>
2022-09-21 22:48:25 -0500

    add stripe_product_id to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_031343) do
+ActiveRecord::Schema.define(version: 2022_09_22_032826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_09_22_031343) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "stripe_product_id"
   end
 
   create_table "users", force: :cascade do |t|
