91cbc7dab400962f2ebf9e572f4fb5dc4ae74973
Santiago Vanegas <santiago.vanegas@ideaware.co>
2022-09-23 00:15:30 -0500

    empty cart after successful transaction

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,14 +1,10 @@
 class CheckoutController < ApplicationController
     
     def create
-        product = Product.find(params[:id])
         @session = Stripe::Checkout::Session.create({
             customer: current_user.stripe_customer_id,
             payment_method_types: ['card'],
-            line_items: [{
-                price: product.stripe_price_id,
-                quantity: 1
-            }],
+            line_items: @cart.collect { |item| item.to_builder.attributes! },
             mode: 'payment',
             success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
             cancel_url: cancel_url,
@@ -20,9 +16,15 @@ class CheckoutController < ApplicationController
     end
 
     def success
-        @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["payment_intent", "line_items"] })
-        @session_with_expand.line_items.data.each do |line_item|
-            product = Product.find_by(stripe_product_id: line_item.price.product)
+        if params[:session_id].present?
+            # session.delete(:cart)
+            session[:cart] = []
+            @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["payment_intent", "line_items"] })
+            @session_with_expand.line_items.data.each do |line_item|
+                product = Product.find_by(stripe_product_id: line_item.price.product)
+            end
+        else
+            redirect_to cancel_url, alert: "No info to display"
         end
     end
 
